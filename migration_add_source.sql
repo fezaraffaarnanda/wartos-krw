c8762e3db6d794bbea1a5af10eb4ce118edf6b79
Feza Raffa Arnanda <fezaarnand@gmail.com>
2026-03-05 14:28:25 +0700

    remove sql

--- migration_add_source.sql
+++
@@ -1,4 +0,0 @@
--- Migrasi: tambah kolom source ke tabel berita
--- Jalankan di Supabase SQL Editor jika tabel sudah ada sebelumnya
-
-ALTER TABLE berita ADD COLUMN IF NOT EXISTS source TEXT DEFAULT 'Radar Tegal';
